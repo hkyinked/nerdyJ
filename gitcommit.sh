6f85047c85c8afcbff307d5accf3498512c13d24
hkyinked <hkyinked@gmail.com>
2019-05-18 22:10:17 -0400

    commitpush

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 #first checks status of git repo
-git status
+if [ "$(git status --porcelain)" ]; then 
+#working directory clean
+  
 #next adds all commits
 git add .
+sleep 3
+
 #asks usr for commit title
-echo 'what are you commiting today?' 
-
-read -p  commit-tag #reads users input and saves it to the varible named "commit-tag"]
-sleep 3
-git commit -m "commit-tag"
-
+echo 'what are you commiting today?'
+read commitpush #reads users input and saves it to the varible named "commitpush"]
+git commit -m "commitpush"
+sleep 2
 #git push commit data to github repo
-
 git push origin master
-sleep 3
-
-read -p 'username: ' uservar
-sleep 1
-
-read -sp 'Password: ' passvar
-sleep 1
-
+sleep 4
+#read -p 'username: ' uservar
+#read -sp 'Password: ' passvar
 echo thanks for your contribution today
+fi
